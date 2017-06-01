350042142dd31f810ff9d4e618d25f59e245daea
Your Name <you@example.com>
2017-06-01 15:13:45 +0200

    cos wiecej

---
+++ warunkowa.js
@@ -0,0 +1,14 @@
+var a = 10;
+var b = 20;
+var value;
+value = (a * a) + (2 * a * b) - (b * b);
+console.log(value);
+
+if (value > 0) {
+	console.log("Wieksze od zera, jesteś wspanialy, jestes wlochaty");
+}	else if (value < 0}	{
+	console.log("Mniej niż zerooo o");
+}	else {
+	console.log("Nie wiem. Co to ma byc?!");
+}
+
