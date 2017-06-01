b8e4bc0e82602e3893b65970b894ca0a269966e3
Your Name <you@example.com>
2017-06-01 17:59:34 +0200

    zmiana2

--- warunkowa.js
+++ warunkowa.js
@@ -1,4 +1,4 @@
-var a = 10;
+﻿var a = 10;
 var b = 20;
 var value;
 value = (a * a) + (2 * a * b) - (b * b);
@@ -8,7 +8,7 @@ if (value > 0) {
 	console.log("Wieksze od zera, jesteś wspanialy, jestes wlochaty");
 }	else if (value < 0}	{
 	console.log("Mniej niż zero");
-}	else if (value = 0){
+}	else if (value == 0){
 	console.log("Wynik jest równy zero");
 }
 
